2a8cc57326e4cacc2268b55eb712f588cdd566ca
Dong Hyuk Park <sts04038@gmail.com>
2023-05-30 11:51:17 +0900

    5/30 js 실습용 3교시 end

--- default0530.js
+++ default0530.js
@@ -1,1 +1,28 @@
-document.write("Hello, world")
+//document.write("Hello, world")
+//document.write("<h1> Welcome to JS Program</h1>")
+//document.write("<h2> Welcome to JS Program</h2>")
+//
+//console.log('Welcome JS Program');
+//console.info('Welcome JS Program');
+//console.warn('Welcome JS Program');
+//console.error('Welcome JS Program');
+//
+//alert('Welcome JS Program') // 팝업창 뜸 - 웬만하면 사용하지 않음 - 제일 먼저 실행되고 사용자 당황함
+//var a = prompt('Welcome to JS Program');
+//console.log(a)
+
+//typeof - 변수 타입 알려줌
+console.log(typeof 123);            // number
+console.log(123.5, typeof 123.5);   // 123.5, "number"
+console.log(typeof "123");          // string
+console.log(typeof true);           // boolean
+console.log(typeof false);          // boolean
+
+var car;
+var car = "";
+
+var person
+var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+console.log(person)
+console.log(typeof person); // object
+person = null;
