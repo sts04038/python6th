97ccadcf2a1adaf0a6b50f6b47899b669099f2d0
Dong Hyuk Park <sts04038@gmail.com>
2023-05-30 13:52:55 +0900

    5/30 js 실습용  4교시 end

--- default0530.js
+++ default0530.js
@@ -12,17 +12,51 @@
 //console.log(a)
 
 //typeof - 변수 타입 알려줌
-console.log(typeof 123);            // number
-console.log(123.5, typeof 123.5);   // 123.5, "number"
-console.log(typeof "123");          // string
-console.log(typeof true);           // boolean
-console.log(typeof false);          // boolean
+//console.log(typeof 123);            // number
+//console.log(123.5, typeof 123.5);   // 123.5, "number"
+//console.log(typeof "123");          // string
+//console.log(typeof true);           // boolean
+//console.log(typeof false);          // boolean
+//
+//var car;
+//var car = "";
+//
+//var person
+//var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+//console.log(person)
+//console.log(typeof person); // object
+//person = null;
 
-var car;
-var car = "";
+// 4교시 시작
+var name = "이승훈"
+var age = 29;
+var cgpa = 3.92;
+var lineBreak = "<br/>"
 
-var person
-var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
-console.log(person)
-console.log(typeof person); // object
-person = null;
+document.write("이름: " + name + lineBreak);
+document.write("나이: " + age + lineBreak);
+document.write("학점: " + cgpa + lineBreak);
+
+var text = prompt("Enter your name");
+document.write("Your name : " + text + "<br/>");
+
+var len = text.length;         // 전체 글자수 띄어쓰기 포함됨
+document.write("Number of characters : " + len + "<br/>");
+
+document.write(text.charAt(2) + "<br/>");
+document.write(text.toUpperCase() + "<br/>");
+document.write(text.toLowerCase() + "<br/>");
+
+var text1 = "hi ";
+var text2 = "bye";
+// + 기호와 concat은 같은 기능을 한다.
+var text3 = text1.concat(text2);     //hi bye
+var text4 = text1 + text2;           //hi bye
+document.write(text3 + "<br/>");
+document.write(text4 + "<br/>");
+
+var text5 = "hello";
+var result = text5.slice(0,2);      // he 출력됨 - 0,1번째 글짜 출력
+var result = text5.slice(2);      // llo 출력됨 - 0,1번째 글짜 출력
+document.write(result + "<br/>");
+
