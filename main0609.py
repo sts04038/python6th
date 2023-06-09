ac29d3b03d46d76e53d956b90cecd445c685c979
Dong Hyuk Park <sts04038@gmail.com>
2023-06-09 14:02:54 +0900

    6/9 gitignore 파일 추가

--- main0609.py
+++ main0609.py
@@ -1,6 +1,19 @@
-file_object = open('example.txt', 'w')
+# file_object = open('example.txt', 'r')
+#
+# position = file_object.tell()
+# print("Current position: ", position)
+#
+# file_object.seek(7)
+#
+# position = file_object.tell()
+# print("Changed position: ", position)
+#
+# file_object.close()
 
-content = "This is a new file.\nPython is fun"
-file_object.write(content)
+with open('example.txt', 'w') as file_object:
+    content = """This is a multiline string.
+Python is a versatile language.
+It is easy to learn and use."""
+    print(content)
+    file_object.write(content)
 
-file_object.close()
