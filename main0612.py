c076b103aa90155c1b9e07755a54313d0695a64d
Dong Hyuk Park <sts04038@gmail.com>
2023-06-12 13:18:02 +0900

    6/12 에러 exception

--- main0612.py
+++ main0612.py
@@ -1,41 +1,28 @@
-# class Car:
-#     wheels = 4
+# from MyApp.Handlers.text_handler import handle_text
 #
-#     def __init__(self, make, model, color):
-#         self.make = make
-#         self.model = model
-#         self.color = color
-#
-#     # method
-#     def drive(self):
-#         return "The car is moving"
-#     def stop(self):
-#         return "The car has stopped."
-#
-# my_car = Car("Kia", "Morning", "Blue")
-#
-# # 속성 사용
-# print(my_car.make)      # Kia
-# # 메소드 호출
-# print(my_car.drive())   # The car is moving
-# print(my_car.stop())    # The car has stopped.
+# input_text = "python package practices"
+# handle_text(input_text) # PYTHON PACKAGE PRACTICES
 
-class Vehicle:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("Error: Division by zero")
 
-    def start_engine(self):
-        return "The enging is running!"
+print("Program continues.")
 
-# 자식 클래스
-class Car(Vehicle):
-    def start_engine(self):
-        return super().start_engine() + " It's a car engine"
+try:
+    number = int("Not a number")
+    number1 = 5 + "Not a number"
+except ValueError:
+    print("Error: Invalid value")
+except TypeError:
+    print("Error: invalid type")
 
-# 인스턴스 생성
-my_car = Car("Toyota", "Corolla", 2020)
+class CustomException(Exception):
+    def __init__(self, message):
+        self.message = message
 
-#메소드 출력
-print(my_car.start_engine())
+try:
+    raise CustomException("This is a custom exception. ")
+except CustomException as e:
+    print(f"Error: {e.message}")
