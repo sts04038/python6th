477fdb49483abc0d1f2415dc77e11f7076e68091
Dong Hyuk Park <sts04038@gmail.com>
2023-06-12 10:09:49 +0900

    6/9 gitignore 파일 추가

--- main0609.py
+++ main0609.py
@@ -10,10 +10,28 @@
 #
 # file_object.close()
 
-with open('example.txt', 'w') as file_object:
-    content = """This is a multiline string.
-Python is a versatile language.
-It is easy to learn and use."""
-    print(content)
-    file_object.write(content)
+# with open('example.txt', 'w') as file_object:
+#     content = """This is a multiline string.
+# Python is a versatile language.
+# It is easy to learn and use."""
+#     print(content)
+#     file_object.write(content)
 
+# file_object = open('list_example.txt', 'w')
+# content_list = ["Python", "Java", "C++", "Javascript"]
+#
+# for item in content_list:
+#     file_object.write(item + '\n')
+#
+# file_object.close()
+
+import os
+current_directory = os.getcwd()
+print(current_directory)
+
+os.makedirs('parent_directory/child_directory/grandchild_directory')
+
+for dirpath, dirnames, filenames in os.walk('parent_directory'):
+    print(f"디렉터리 경로: {dirpath}")
+    print(f"디렉터리 이름: {dirnames}")
+    print(f"파일 이름: {filenames}")
