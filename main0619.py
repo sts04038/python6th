a0a75785224a0ba3696f1529c6e317339311b7d0
Dong Hyuk Park <sts04038@gmail.com>
2023-06-19 14:16:34 +0900

    urllib request

--- main0619.py
+++ main0619.py
@@ -1,39 +1,9 @@
-from collections import deque
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
+import urllib.request
 
-visited = set()
+url = 'https://www.google.com'
+request = urllib.request.Request(url)
+response = urllib.request.urlopen(request)
 
-# 너비 우선 탐색
-def bfs_iterative(start_node):
-    queue = deque([start_node])
+html = response.read()
 
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            queue.extend(graph[node])
-
-# 깊이 우선 탐색
-def dfs_iterative(start_node):
-    stack = [start_node]
-
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            stack.extend(reversed(graph[node]))
-
-snode = 'A'
-dfs_iterative(snode) # output: A B D E F C
-visited.clear() # 초기화
-print('\n-------')
-bfs_iterative(snode) # output: A B C D E F
+print(html)
